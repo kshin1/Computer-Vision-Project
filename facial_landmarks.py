ab8fde0156d0c2d0ffd0bd278fbf30c8d2cd669d
Mimi Chen <mchen6@nd.edu>
2017-11-21 17:06:19 -0800

    Added user input tagging

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -1,6 +1,3 @@
-# USAGE
-# python facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_01.jpg 
-
 # import the necessary packages
 from imutils import face_utils
 import numpy as np
@@ -9,96 +6,149 @@ import imutils
 import dlib
 import cv2
 import glob
-#cv_img_train = []
-#cv_img_test = []
-#for img in glob.glob("../projectData/[A-J][0-9]*.bmp"):
-#    n= cv2.imread(img)
-#    cv_img_train.append(n)
-#for img in glob.glob("../projectData/[K-M][0-9]*.bmp"):
-#    n= cv2.imread(img)
-#    cv_img_test.append(n)
+import pickle
+import os
 
-# construct the argument parser and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-p", "--shape-predictor", required=True,
-#	help="path to facial landmark predictor")
-#ap.add_argument("-i", "--image", required=True,
-#	help="path to input image")
-#args = vars(ap.parse_args())
+happy = {}
+sad = {}
+angry = {}
+neutral = {}
+surprised = {}
+confused = {}
+other = {}
+
+EXPRESSION_DICT = {"h": happy, "sa": sad, "a": angry,\
+		"n": neutral, "su": surprised, "c": confused, "o": other}
+EXPRESSION = {"h":"happy", "sa": "sad", "a": "angry",\
+		"n": "neutral", "su": "surprised", "c": "confused", "o": "other"}
+
+for key, d in EXPRESSION_DICT.items():
+	if os.path.isfile(EXPRESSION[key] + ".pickle"):
+		f = open(EXPRESSION[key] + ".pickle", "rb")
+		d = pickle.load(f)
+		f.close()
+
+for d in EXPRESSION_DICT.values():
+	print(d)
 
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
 detector = dlib.get_frontal_face_detector()
-#predictor = dlib.shape_predictor(args["shape_predictor"])
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 # load the input image, resize it, and convert it to grayscale
 #for img in glob.glob("../projectData/*.bmp"): #THIS IS FOR ALL IMAEGS
-for img in glob.glob("projectImages/[A-J][0-9]*.bmp"): #THIS IS FOR TRAINING DATA
-    #image = cv2.imread(args["image"])
-    image = cv2.imread(img)
-    image = imutils.resize(image, width=500)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-    
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-    	# determine the facial landmarks for the face region, then
-    	# convert the facial landmark (x, y)-coordinates to a NumPy
-    	# array
-    	shape = predictor(gray, rect)
-    	shape = face_utils.shape_to_np(shape)
-    
-    	# convert dlib's rectangle to a OpenCV-style bounding box
-    	# [i.e., (x, y, w, h)], then draw the face bounding box
-    	(x, y, w, h) = face_utils.rect_to_bb(rect)
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    	# show the face number
-    	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
-    	# loop over the (x, y)-coordinates for the facial landmarks
-    	# and draw them on the image
-    	for (x, y) in shape:
-    		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-    
-    # show the output image with the face detections + facial landmarks
-    cv2.imshow("Output", image)
-    cv2.waitKey(0)
+#for img in glob.glob("projectImages/[A-J][0-9]*.bmp"): #THIS IS FOR TRAINING DATA
+for img in glob.glob("projectImages/A[0-9]*.bmp"): # Try on test subject 1
+	img_id = img.split("/")[1]
+	# Check if img has already been tagged
+	tagged = False
+	for key, d in EXPRESSION_DICT.items():
+		# Already tagged the image
+		if img_id in d.keys():
+			tagged = True
+			break
+	# Go to the next image
+	if tagged:
+		continue
 
-for img in glob.glob("projectImages/[K-M][0-9]*.bmp"): #THIS IS FOR TEST DATA
-    #image = cv2.imread(args["image"])
-    image = cv2.imread(img)
-    image = imutils.resize(image, width=500)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-    
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-    	# determine the facial landmarks for the face region, then
-    	# convert the facial landmark (x, y)-coordinates to a NumPy
-    	# array
-    	shape = predictor(gray, rect)
-    	shape = face_utils.shape_to_np(shape)
-    
-    	# convert dlib's rectangle to a OpenCV-style bounding box
-    	# [i.e., (x, y, w, h)], then draw the face bounding box
-    	(x, y, w, h) = face_utils.rect_to_bb(rect)
-    	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    	# show the face number
-    	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-    		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
-    	# loop over the (x, y)-coordinates for the facial landmarks
-    	# and draw them on the image
-    	for (x, y) in shape:
-    		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-    
-    # show the output image with the face detections + facial landmarks
-    cv2.imshow("Output", image)
-    cv2.waitKey(1)
+	image = cv2.imread(img)
+	image = imutils.resize(image, width=500)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+	# detect faces in the grayscale image
+	rects = detector(gray, 1)
+	
+	# loop over the face detections
+	for (i, rect) in enumerate(rects):
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+	
+		# convert dlib's rectangle to a OpenCV-style bounding box
+		# [i.e., (x, y, w, h)], then draw the face bounding box
+		(x, y, w, h) = face_utils.rect_to_bb(rect)
+		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	
+		# show the face number
+		cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+	
+		# loop over the (x, y)-coordinates for the facial landmarks
+		# and draw them on the image
+		for (x, y) in shape:
+			cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+	
+	# show the output image with the face detections + facial landmarks
+	cv2.imshow("Output", image)
+
+	cv2.waitKey(0)
+	
+	facial_features = {}
+	for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+		facial_features[name] = shape[i:j]
+	
+	# User tagging facial expression
+	expression = raw_input("{}: ".format(img_id))
+	while expression not in EXPRESSION_DICT.keys():
+		expression = raw_input("{}: ".format(list(EXPRESSION_DICT.keys())))
+	
+	EXPRESSION_DICT[expression][img_id] = facial_features
+	
+	if False:
+		if expression == "h": 
+			happy[img_id] = facial_features
+		elif expression == "sa":
+			sad[img_id] = facial_features
+		elif expression == "a":
+			angry[img_id] = facial_features
+		elif expression == "n":
+			neutral[img_id] = facial_features
+		elif expression == "c":
+			confused[img_id] = facial_features
+		elif expression == "o":
+			other[img_id] = facial_features
+
+
+# Save classifications to JSON files
+for e, d in EXPRESSION_DICT.items():
+	f = open(EXPRESSION[e] + ".pickle", "wb")
+	pickle.dump(d, f)
+	f.close()
+
+if False:
+	for img in glob.glob("projectImages/[K-M][0-9]*.bmp"): #THIS IS FOR TEST DATA
+		#image = cv2.imread(args["image"])
+		image = cv2.imread(img)
+		image = imutils.resize(image, width=500)
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		
+		# detect faces in the grayscale image
+		rects = detector(gray, 1)
+		
+		# loop over the face detections
+		for (i, rect) in enumerate(rects):
+			# determine the facial landmarks for the face region, then
+			# convert the facial landmark (x, y)-coordinates to a NumPy
+			# array
+			shape = predictor(gray, rect)
+			shape = face_utils.shape_to_np(shape)
+		
+			# convert dlib's rectangle to a OpenCV-style bounding box
+			# [i.e., (x, y, w, h)], then draw the face bounding box
+			(x, y, w, h) = face_utils.rect_to_bb(rect)
+			cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		
+			# show the face number
+			cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		
+			# loop over the (x, y)-coordinates for the facial landmarks
+			# and draw them on the image
+			for (x, y) in shape:
+				cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+		
+		# show the output image with the face detections + facial landmarks
+		cv2.imshow("Output", image)
+		cv2.waitKey(1)
