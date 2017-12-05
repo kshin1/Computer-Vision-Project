de165416a7f9de8e34a8af1b2d90f63ab133ad04
Mimi Chen <mchen6@nd.edu>
2017-12-04 22:12:11 -0500

    Added confusion matrix

--- svm_facial.py
+++ svm_facial.py
@@ -5,23 +5,69 @@ import sklearn as skl
 import os
 from sklearn.model_selection import train_test_split
 from sklearn import svm
-import sklearn.metrics as skimetrics
+import sklearn.metrics as sklmetrics
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv("emotions.csv")
-data_X = data.drop("emotion", axis = 1)
-data_Y = data["emotion"]
+EXPRESSION = {"h":"happy", "sa": "sad", "a": "angry",\
+				"n": "neutral", "su": "surprised", "c": "confused", "o": "other"}
 
-# Split to training and testing data
-train_X, test_X, train_Y, test_Y = train_test_split(data_X, data_Y, random_state = 42, train_size = 0.7)
+def camera_loop():
+    print("Press <SPACE> to capture/classify an image, or <Esc> to exit.")
+    cap = cv2.VideoCapture(0)
+    while (True):
+        _, frame = cap.read()
 
-# Create a SVM 
-clf = svm.SVC()
+        action = cv2.waitKey(1)
 
-# Fit the model using the training data
-clf.fit(train_X, train_Y)
+        cv2.imshow('camera', frame)
 
-# Use the model to predict the emotions of testing data
-predicted_Y = clf.predict(test_X)
+        if action == ord('q') or action == 27:
+            break
 
-# Print accuracy of the model 
-print skimetrics.accuracy_score(test_Y, predicted_Y)
+        if action == ord(' '):
+            # svm object detection
+            frame = classify_svm(frame)
+            cv2.imshow('SVM output:', frame)
+
+    cap.release()
+
+if __name__ == "__main__":
+	data = pd.read_csv("emotions.csv")
+	data_X = data.drop("emotion", axis = 1)
+	data_Y = data["emotion"]
+
+	# Split to training and testing data
+	train_X, test_X, train_Y, test_Y = train_test_split(data_X, data_Y, random_state = 42, train_size = 0.7)
+
+	# Use stored SVM
+	if os.path.isfile("svm.pickle"):
+		f = open("svm.pickle", "rb")
+		clf = pickle.load(f)
+		f.close()
+	# Create a SVM if it does not already exist
+	else:
+		clf = svm.SVC(kernel='poly',degree=3)
+
+		# Fit the model using the training data
+		clf.fit(train_X, train_Y)
+
+		# Save our model to a pickle file 
+		f = open("svm.pickle", "wb")
+		pickle.dump(clf, f)
+		f.close()
+
+	# Use the model to predict the emotions of testing data
+	predicted_Y = clf.predict(test_X)
+	
+	# Print accuracy of the model 
+	print("Accuracy of the model: {}".format(str(sklmetrics.accuracy_score(test_Y, predicted_Y))))
+
+	# Create a confusion matrix
+	conf_mat = sklmetrics.confusion_matrix(test_Y, predicted_Y, labels = list(EXPRESSION.keys()))
+	sns.heatmap(conf_mat, square=True, annot=True, cbar = False, xticklabels = list(EXPRESSION.keys()), yticklabels = list(EXPRESSION.keys()))
+	plt.xlabel("Predicted Value")
+	plt.ylabel("True Value")
+	plt.title("Emotion Detection Accuracy")
+	plt.show()
